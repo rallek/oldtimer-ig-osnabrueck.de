52b505c308cb96fc11de744b5cbfe322388c3da8
rallek <ralf@familie-koester.de>
2017-01-13 17:20:38 +0100

    latest MOST

--- generatedModules/DownloadModule/RK/DownLoadModule/Twig/Base/AbstractTwigExtension.php
+++ generatedModules/DownloadModule/RK/DownLoadModule/Twig/Base/AbstractTwigExtension.php
@@ -121,6 +121,36 @@ abstract class AbstractTwigExtension extends Twig_Extension
     
     
     /**
+     * The rkdownloadmodule_fileSize filter displays the size of a given file in a readable way.
+     * Example:
+     *     {{ 12345|rkdownloadmodule_fileSize }}
+     *
+     * @param integer $size     File size in bytes
+     * @param string  $filepath The input file path including file name (if file size is not known)
+     * @param boolean $nodesc   If set to true the description will not be appended
+     * @param boolean $onlydesc If set to true only the description will be returned
+     *
+     * @return string File size in a readable form
+     */
+    public function getFileSize($size = 0, $filepath = '', $nodesc = false, $onlydesc = false)
+    {
+        if (!is_numeric($size)) {
+            $size = (int) $size;
+        }
+        if (!$size) {
+            if (empty($filepath) || !file_exists($filepath)) {
+                return '';
+            }
+            $size = filesize($filepath);
+        }
+        if (!$size) {
+            return '';
+        }
+    
+        return $this->getReadableFileSize($size, $nodesc, $onlydesc);
+    }
+    
+    /**
      * Display a given file size in a readable format
      *
      * @param string  $size     File size in bytes
