ab84b395c9c080e79209810acbd94d602ea91cbb
rallek <ralf@familie-koester.de>
2017-06-21 17:28:50 +0200

    New Theme X2017

---
+++ src/themes/Rk/X2017Theme/RkX2017Theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ *
+ * Copyright Pfarreiengemeinschaft Georgsmarienhuette Ost - http://pggo.de/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rk\X2017Theme;
+
+use Zikula\Bundle\CoreBundle\Bundle\AbstractCoreTheme;
+
+class RkX2017Theme extends AbstractCoreTheme
+{
+}
